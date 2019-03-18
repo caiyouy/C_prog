eebc910e311fe014dc4eb130a293f8aa567bf008
caiyouy <caiyouy@gmail.com>
2019-03-18 23:08:42 +0800

    Finish

---
+++ 03/code/var_uninit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+
+
+int main()
+{
+        //Define variables
+        int a;
+        double b;
+        char c;
+        //...
+        //Forget variable assignment
+        //
+        printf("a: %d\n",a);
+        printf("b: %f\n",b);
+        printf("c: %c\n",c);
+        return 0;
+}
+
